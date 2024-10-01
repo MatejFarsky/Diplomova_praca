f2e438a3dacdcf757e6758626b3cea23b479eeaf
MatejFarsky <133667281+MatejFarsky@users.noreply.github.com>
2024-10-01 14:36:45 +0200

    Bateria_AMPL.py

--- Bateria_AMPL.py
+++ Bateria_AMPL.py
@@ -7,82 +7,96 @@ from amplpy import AMPL, Environment, DataFrame
 def round_dw(x, dec):
     return math.floor(x * 10**dec) / 10**dec
 
-start = time.time()
+def load_data():
+    df1 = pd.read_excel('Data_input.xlsx')
+    return df1.loc[:,"price"], df1.loc[:,"buy_prev"], df1.loc[:,"sell_prev"]
 
-#Presnost
-dec = 2
+def solve_battery(n, price, buy_prev, sell_prev, cap_0, cap_n, Lower_cap, Upper_cap, Upper_buy, Upper_sell, alpha, beta, c):
+    ampl = AMPL(Environment('C:\\Users\\Matej\\AMPL'))
+    ampl.read('bateria.mod')
+    
+    ampl.getParameter('n').setValues([n])
+    ampl.getParameter('price').setValues(price)
+    ampl.getParameter('buy_prev').setValues(buy_prev)
+    ampl.getParameter('sell_prev').setValues(sell_prev)
+    ampl.getParameter('cap_0').setValues([cap_0])
+    ampl.getParameter('cap_n').setValues([cap_n])
+    ampl.getParameter('Lower_cap').setValues([Lower_cap])
+    ampl.getParameter('Upper_cap').setValues([Upper_cap])
+    ampl.getParameter('Upper_buy').setValues([Upper_buy])
+    ampl.getParameter('Upper_sell').setValues([Upper_sell])
+    ampl.getParameter('alpha').setValues([alpha])
+    ampl.getParameter('beta').setValues([beta])
+    ampl.getParameter('c').setValues([c])
+    
+    ampl.solve()
+    
+    vari = ampl.getVariable('cap').getValues().toList()
+    cap_opt = [vari[t][1] for t in range(len(vari))]
+    vari = ampl.getVariable('buy_cycles').getValues().toList()
+    buy_cycles_opt = [vari[t][1] for t in range(len(vari))]
+    vari = ampl.getVariable('sell_cycles').getValues().toList()
+    sell_cycles_opt = [vari[t][1] for t in range(len(vari))]
+    
+    vari = ampl.getVariable('buy').getValues().toList()
+    buy_opt = [vari[t][1] for t in range(len(vari))]
+    vari = ampl.getVariable('sell').getValues().toList()
+    sell_opt = [vari[t][1] for t in range(len(vari))]
+    vari = ampl.getVariable('buy_or_sell').getValues().toList()
+    buy_or_sell_opt = [vari[t][1] for t in range(len(vari))]
+    
+    opt_val = ampl.get_value('obj_fun')
 
-#Nacitanie dat
-df1 = pd.read_excel('Data_input.xlsx')
-p = df1.loc[:,"p"]
-b = df1.loc[:,"b"]
-d = df1.loc[:,"d"]
+    return cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt
 
-#Definovanie parametrov
-alpha, beta = 0.92, 0.93
-vykon = 1
-a_0 = 0.5   #pociatocna podmienka
-a_n = 0.5   #koncova podmienka
-L_x, U_x = 0, 1
-U_v, U_u = round_dw(vykon / 4 * beta, dec), round_dw(vykon / (4 * alpha), dec)
-c = 2       #max pocet cyklov
-n = len(p)
+def save_data():
+    df1 = pd.read_excel('Data_input.xlsx')
+    buy_prev = df1.loc[:,"buy_prev"].tolist()
+    sell_prev = df1.loc[:,"sell_prev"].tolist()
+    df2 = df1
+    df2 = df2.drop(df2.columns[0], axis=1)
+    df2.buy_prev = pd.Series(np.add(buy_opt, buy_prev))
+    df2.sell_prev = pd.Series(np.add(sell_opt, sell_prev))
+    df2.to_excel('Data_output.xlsx')
 
-#Solving
-ampl = AMPL(Environment('C:\\Users\\Matej\\AMPL'))
-ampl.read('bateria.mod')
+def iteration()
+    start = time.time()
+    
+    #Presnost
+    dec = 2
+    
+    #Nacitanie dat
+    price, buy_prev, sell_prev = load_data()
+    
+    #Definovanie parametrov
+    alpha, beta = 0.92, 0.93
+    vykon = 1
+    cap_0 = 0.5   #pociatocna podmienka
+    cap_n = 0.5   #koncova podmienka
+    Lower_cap, Upper_cap = 0, 1
+    Upper_sell, Upper_buy = round_dw(vykon / 4 * beta, dec), round_dw(vykon / (4 * alpha), dec)
+    c = 2       #max pocet cyklov
+    n = len(price)
+    
+    #Solving
+    cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt = solve_battery(n, price, buy_prev, sell_prev, cap_0, cap_n, Lower_cap, Upper_cap, Upper_buy, Upper_sell, alpha, beta, c)
+    
+    #ulozenie dat
+    save_data()
+    
+    stop = time.time()
+    time = stop - start
 
-ampl.getParameter('n').setValues([n])
-ampl.getParameter('p').setValues(p)
-ampl.getParameter('b').setValues(b)
-ampl.getParameter('d').setValues(d)
-ampl.getParameter('a_0').setValues([a_0])
-ampl.getParameter('a_n').setValues([a_n])
-ampl.getParameter('L_x').setValues([L_x])
-ampl.getParameter('U_x').setValues([U_x])
-ampl.getParameter('U_u').setValues([U_u])
-ampl.getParameter('U_v').setValues([U_v])
-ampl.getParameter('alpha').setValues([alpha])
-ampl.getParameter('beta').setValues([beta])
-ampl.getParameter('c').setValues([c])
+    return cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, time
 
-ampl.solve()
+cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, time = iteration()
 
-vari = ampl.getVariable('x').getValues().toList()
-x_opt = [vari[t][1] for t in range(len(vari))]
-vari = ampl.getVariable('y1').getValues().toList()
-y1_opt = [vari[t][1] for t in range(len(vari))]
-vari = ampl.getVariable('y2').getValues().toList()
-y2_opt = [vari[t][1] for t in range(len(vari))]
-
-vari = ampl.getVariable('u').getValues().toList()
-u_opt = [vari[t][1] for t in range(len(vari))]
-vari = ampl.getVariable('v').getValues().toList()
-v_opt = [vari[t][1] for t in range(len(vari))]
-vari = ampl.getVariable('w').getValues().toList()
-w_opt = [vari[t][1] for t in range(len(vari))]
-
-opt_val = ampl.get_value('obj_fun')
-
-print("x_opt: ", x_opt)
-print("y1_opt: ", y1_opt)
-print("y2_opt: ", y2_opt)
-print("u_opt: ", u_opt)
-print("v_opt: ", v_opt)
-print("w_opt: ", w_opt)
+print("cap_opt: ", cap_opt)
+print("buy_cycles_opt: ", buy_cycles_opt)
+print("sell_cycles_opt: ", sell_cycles_opt)
+print("buy_opt: ", buy_opt)
+print("sell_opt: ", sell_opt)
+print("buy_or_sell_opt: ", buy_or_sell_opt)
 print("opt_val:", opt_val)
-
-b = df1.loc[:,"b"].tolist()
-d = df1.loc[:,"d"].tolist()
-
-#ulozenie dat
-df2 = df1
-df2 = df2.drop(df2.columns[0], axis=1)
-df2.b = pd.Series(np.add(u_opt, b))
-df2.d = pd.Series(np.add(v_opt, d))
-df2.to_excel('Data_output.xlsx')
-
-
-stop = time.time()
-print("Seconds elapsed: ",stop - start)
-#Seconds elapsed:  89.15838241577148
+print("Seconds elapsed: ", time)
+#Seconds elapsed:  
