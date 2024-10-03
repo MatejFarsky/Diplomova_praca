0e2dd0d14db2348a783e7b8ae95363734a3e2578
MatejFarsky <133667281+MatejFarsky@users.noreply.github.com>
2024-10-03 18:57:06 +0200

    Bateria_AMPL.py

--- Bateria_AMPL.py
+++ Bateria_AMPL.py
@@ -1,5 +1,5 @@
 import math
-import time
+import time as tm
 import numpy as np
 import pandas as pd
 from amplpy import AMPL, Environment, DataFrame
@@ -47,9 +47,9 @@ def solve_battery(n, price, buy_prev, sell_prev, cap_0, cap_n, Lower_cap, Upper_
     
     opt_val = ampl.get_value('obj_fun')
 
-    return cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt
+    return cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, opt_val
 
-def save_data():
+def save_data(buy_opt, sell_opt, cap_opt):
     df1 = pd.read_excel('Data_input.xlsx')
     buy_prev = df1.loc[:,"buy_prev"].tolist()
     sell_prev = df1.loc[:,"sell_prev"].tolist()
@@ -57,10 +57,13 @@ def save_data():
     df2 = df2.drop(df2.columns[0], axis=1)
     df2.buy_prev = pd.Series(np.add(buy_opt, buy_prev))
     df2.sell_prev = pd.Series(np.add(sell_opt, sell_prev))
+    df2.insert(len(df2.columns),'buy_opt', pd.Series(buy_opt))
+    df2.insert(len(df2.columns), 'sell_opt', pd.Series(sell_opt))
+    df2.insert(len(df2.columns), 'cap_opt', pd.Series(cap_opt))
     df2.to_excel('Data_output.xlsx')
 
-def iteration()
-    start = time.time()
+def iteration(tm):
+    start = tm.time()
     
     #Presnost
     dec = 2
@@ -74,22 +77,22 @@ def iteration()
     cap_0 = 0.5   #pociatocna podmienka
     cap_n = 0.5   #koncova podmienka
     Lower_cap, Upper_cap = 0, 1
-    Upper_sell, Upper_buy = round_dw(vykon / 4 * beta, dec), round_dw(vykon / (4 * alpha), dec)
+    Upper_sell, Upper_buy = round_dw(vykon / 4 * beta, dec), round_dw(vykon / 4, dec)
     c = 2       #max pocet cyklov
     n = len(price)
     
     #Solving
-    cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt = solve_battery(n, price, buy_prev, sell_prev, cap_0, cap_n, Lower_cap, Upper_cap, Upper_buy, Upper_sell, alpha, beta, c)
+    cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, opt_val = solve_battery(n, price, buy_prev, sell_prev, cap_0, cap_n, Lower_cap, Upper_cap, Upper_buy, Upper_sell, alpha, beta, c)
     
     #ulozenie dat
-    save_data()
+    save_data(buy_opt, sell_opt, cap_opt)
     
-    stop = time.time()
+    stop = tm.time()
     time = stop - start
 
-    return cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, time
+    return cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, opt_val, time
 
-cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, time = iteration()
+cap_opt, buy_cycles_opt, sell_cycles_opt, buy_opt, sell_opt, buy_or_sell_opt, opt_val, time = iteration(tm)
 
 print("cap_opt: ", cap_opt)
 print("buy_cycles_opt: ", buy_cycles_opt)
@@ -99,4 +102,4 @@ print("sell_opt: ", sell_opt)
 print("buy_or_sell_opt: ", buy_or_sell_opt)
 print("opt_val:", opt_val)
 print("Seconds elapsed: ", time)
-#Seconds elapsed:  
+#Seconds elapsed:  0.6950726509094238
